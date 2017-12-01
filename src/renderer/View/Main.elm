28f3e46caf4266ad50d58607ca06698f358c3e95
Guillaume Hivert <hivert.is.coming@gmail.com>
2017-12-01 18:15:33 +0100

    :lipstick: Add header in Main view

--- src/renderer/View/Main.elm
+++ src/renderer/View/Main.elm
@@ -1,8 +1,33 @@
 module View.Main exposing (..)
 
 import Html exposing (Html)
+import Html.Attributes
 import Types exposing (..)
 
 view : Model -> Html Msg
 view model =
-  Html.div [] []
+  Html.div []
+    [ header model ]
+
+header : Model -> Html Msg
+header model =
+  Html.div
+    [ Html.Attributes.class "flex" ]
+    [ searchField model
+    , profileIcon model
+    ]
+
+searchField : Model -> Html Msg
+searchField model =
+  Html.input
+    [ Html.Attributes.type_ "text"
+    , Html.Attributes.placeholder "Search"
+    ]
+    []
+
+profileIcon : Model -> Html Msg
+profileIcon model =
+  Html.div []
+    [ Html.span [] [ Html.text "name" ]
+    , Html.img [] []
+    ]
