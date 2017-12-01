b5cf64405578fda2e468563b4e3f52fd40192833
Guillaume Hivert <hivert.is.coming@gmail.com>
2017-12-01 16:52:38 +0100

    :lipstick: Improve SASS

--- src/main/Process.elm
+++ src/main/Process.elm
@@ -5,6 +5,9 @@ import Electron.BrowserWindow
 
 type alias Model = Int
 
+type Msg
+  = WindowAllClosed
+
 main : Program Never Model msg
 main =
   Electron.program
@@ -20,13 +23,10 @@ init =
       [ ("width", "1000")
       , ("height", "700")
       , ("show", "true")
-      -- , ("transparent", "true")
-      -- , ("frame", "false")
-      -- , ("titleBarStyle", "hiddenInset")
       , ("vibrancy", "selection")
       ]
     ]
--- appearance-based, light, dark, titlebar, selection, menu, popover, sidebar, medium-light or ultra-dark
+
 update : a -> b -> ( b, Cmd msg )
 update msg model =
   model ! []
